9e26cbeec01ac3f85fb2c028dad75bed1b786d20
Sunil Sah <72701779+sunilsah89@users.noreply.github.com>
2020-10-11 13:33:49 -0500

    Complteing 2nd Assignment (#1)
    Co-authored-by: Sunil Sah <poojasah974@gmail.com>

--- src/com/cerotid/assignment/assignment002.java
+++ src/com/cerotid/assignment/assignment002.java
@@ -3,7 +3,11 @@ package com.cerotid.assignment;
 public class assignment002 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		1. Create an account in GitHub									---Done	
+//		2. Create a repository in GitHub								---Done		
+//		3. Push your latest codes to the newly created repo in GitHub	---Done
+//		4. Add me(shagar-upadhyay) as a collaborator in that repo		---Done
+//		5. Create a new feature branch, push it to GitHub, create a new Pull Request and merge the PR to the main branch ---Done
 
 	}
 
